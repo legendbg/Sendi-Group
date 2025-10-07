67dfd408bc71156b5003b4dd616287256f5ad3f8
yusif641 <gorc141408@gmail.com>
2025-10-07 21:52:11 +0300

    scroll animations

--- js/barAnimation.js
+++ js/barAnimation.js
@@ -1,9 +1,24 @@
 const barAnimation = document.querySelector(".works__content");
+const mediaQueryList = window.matchMedia("(max-width: 1020px)");
 
-barAnimation.addEventListener("mouseenter", () => {
-    barAnimation.style.animationPlayState = "running";
-});
+let isRun = true;
 
-barAnimation.addEventListener("mouseleave", () => {
-    barAnimation.style.animationPlayState = "paused";
-});
+if (mediaQueryList) {
+    barAnimation.addEventListener("click", (el) => {
+        if (isRun) {
+            barAnimation.style.animationPlayState = "paused";
+            isRun = false;
+        } else {
+            barAnimation.style.animationPlayState = "running";
+            isRun = true;
+        }
+    });
+} else {
+    barAnimation.addEventListener("mouseenter", () => {
+        barAnimation.style.animationPlayState = "running";
+    });
+
+    barAnimation.addEventListener("mouseleave", () => {
+        barAnimation.style.animationPlayState = "paused";
+    });
+}
