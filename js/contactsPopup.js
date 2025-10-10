e8ff1510457c5f5071f1bd5767665aaff7b4b2e3
yusif641 <gorc141408@gmail.com>
2025-10-10 13:08:46 +0300

    popup fixed

--- js/contactsPopup.js
+++ js/contactsPopup.js
@@ -5,15 +5,18 @@ const closeContactsPopup = document.querySelector(".feedback-popup__cross");
 contactsPopupButtons.forEach((el) =>
     el.addEventListener("click", (e) => {
         contactsPopup.classList.add("show");
+        document.body.classList.add("active")
     })
 );
 
 closeContactsPopup.addEventListener("click", (e) => {
     contactsPopup.classList.remove("show");
+    document.body.classList.remove("active")
 })
 
 document.addEventListener("click", (e) => {
     if (e.target === contactsPopup) {
         contactsPopup.classList.remove("show");
+        document.body.classList.remove("active")
     }
 })
